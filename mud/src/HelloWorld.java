8dbaaf6abb1b49bc652f38bdef33b76c11763974
Kevin Jolley <serrowpls@gmail.com>
2021-05-01 18:22:32 -0500

    hello world, we're starting again

---
+++ mud/src/HelloWorld.java
@@ -0,0 +1,9 @@
+import console.Player;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Player myPlayer = new Player("Serrow");
+
+        System.out.println(myPlayer.getName());
+    }
+}
---
+++ mud/src/console/Player.java
@@ -0,0 +1,13 @@
+package console;
+
+public class Player {
+    private final String name;
+
+    public Player(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
